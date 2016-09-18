ade193828d912db7f2b69b8682bf00dcdfadad15
askmiller <ajmiller2@mail.widener.edu>
2016-09-17 22:24:06 -0400

    fixed diagnal movement.  Changed all movement to vector based

--- player-movement.gd
+++ player-movement.gd
@@ -1,42 +1,42 @@
 extends Node
 
-var speed = 2
-var acceleration = .5
+var max_speed = 2
 var friction = .25
+var acceleration_magnitude = .5
 var primary_fire_cooldown = 0
 var bullet_scene = load("res://bullet-scene.tscn")
-var vertical_speed = 0
-var horizontal_speed = 0
+var speed_vector = Vector3(0, 0, 0)
+
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	var cube = get_node("player")
-	var location = cube.get_transform()
-	get_node("player").translate(Vector3(horizontal_speed * delta, 0, vertical_speed * delta))
-	
-	if (Input.is_action_pressed("ui_down") and vertical_speed < speed):
-		vertical_speed += acceleration
-	if (Input.is_action_pressed("ui_up") and vertical_speed > -speed):
-		vertical_speed -= acceleration
-	if (Input.is_action_pressed("ui_left") and horizontal_speed < speed):
-		horizontal_speed += acceleration
-	if (Input.is_action_pressed("ui_right") and horizontal_speed > -speed):
-		horizontal_speed -= acceleration
-	if (vertical_speed > 0):
-		vertical_speed -= friction
-	elif (vertical_speed < 0):
-		vertical_speed += friction
-	if (horizontal_speed > 0):
-		horizontal_speed -= friction
-	elif (horizontal_speed < 0):
-		horizontal_speed += friction
-	
+	var player = get_node("player")
+	var acceleration = Vector3(0, 0, 0)
+	if (Input.is_action_pressed("ui_down")):
+		acceleration += Vector3(0, 0, 1)
+	if (Input.is_action_pressed("ui_up")):
+		acceleration -= Vector3(0, 0, 1)
+	if (Input.is_action_pressed("ui_left")):
+		acceleration += Vector3(1, 0, 0)
+	if (Input.is_action_pressed("ui_right")):
+		acceleration -= Vector3(1, 0, 0)
+	acceleration = acceleration.normalized() * acceleration_magnitude
+	var friction_vector = speed_vector.normalized() * friction
+	speed_vector += acceleration - friction_vector
+	if (speed_vector.length() > max_speed):
+		speed_vector = speed_vector.normalized() * max_speed
+	if (abs(speed_vector.x) < friction):
+		speed_vector.x = 0
+	if (abs(speed_vector.z) < friction):
+		speed_vector.z = 0
+	#player.set_rotation(Vector3(PI / 2, 0, PI))
+	player.translate(speed_vector * delta)
 	primary_fire_cooldown -= delta
 	if (Input.is_action_pressed("player_fire_primary") && primary_fire_cooldown <= 0):
 		primary_fire_cooldown = 0.1
 		var bullet = bullet_scene.instance()
 		bullet.velocity = Vector3(0,20,0)
 		get_tree().get_root().add_child(bullet)
-		bullet.set_transform(cube.get_global_transform())
+		bullet.set_transform(player.get_global_transform())
