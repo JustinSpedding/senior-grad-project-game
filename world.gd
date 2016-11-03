6bd2880c1202ea64c4597f4a1d74605d3d2ea259
Justin Spedding <justinspedding@users.noreply.github.com>
2016-11-03 14:48:47 -0400

    Create new rambot every 5 seconds

--- world.gd
+++ world.gd
@@ -3,13 +3,20 @@ extends Spatial
 
 var rambot_scene = load("res://enemies/rambot9001/rambot9001.tscn")
 
+var rambot_spawn_rate = 5
+var rambot_spawn_time_remaining = 0
+
 func _ready():
-	create_rambot()
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	var player_scene = get_node("player_scene")
 	player_scene.translate(Vector3(0, 0, -delta))
+	
+	rambot_spawn_time_remaining -= delta
+	if rambot_spawn_time_remaining <= 0:
+		create_rambot()
+		rambot_spawn_time_remaining = rambot_spawn_rate
 
 func create_rambot():
 	var player_scene = get_node("player_scene")
