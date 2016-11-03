bc05bbba4acc586a881533b3e185caef21009d29
Justin Spedding <justinspedding@users.noreply.github.com>
2016-11-02 22:14:00 -0400

    Replaced player_scene's animation loop with constant velocity and added test cubed to world_scene

--- world.gd
+++ world.gd
@@ -1,19 +1,20 @@
 
 extends Spatial
 
-var rambot_scene = load("res://enemies/rambot9001/rambot9001.tscn");
+var rambot_scene = load("res://enemies/rambot9001/rambot9001.tscn")
 
 func _ready():
 	create_rambot()
-	set_fixed_process(true);
+	set_fixed_process(true)
 
 func _fixed_process(delta):
-	pass;
+	var player_scene = get_node("player_scene")
+	player_scene.translate(Vector3(0, 0, -delta))
 
 func create_rambot():
 	var player_scene = get_node("player_scene")
-	var player = player_scene.get_node("player");
-	var rambot = rambot_scene.instance();
-	rambot.setTarget(player);
-	rambot.translate(Vector3(0, 0, -1000));
-	player_scene.add_child(rambot);
+	var player = player_scene.get_node("player")
+	var rambot = rambot_scene.instance()
+	rambot.setTarget(player)
+	rambot.translate(Vector3(0, 0, -1000))
+	player_scene.add_child(rambot)
