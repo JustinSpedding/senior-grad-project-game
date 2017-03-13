8c2d2ef0d3ada0c5503d247964361675262c724f
Justin Spedding <justinspedding@users.noreply.github.com>
2017-03-13 19:50:49 -0400

    Cleaned up timestamp reading code and added triggers for all event types

--- world.gd
+++ world.gd
@@ -7,22 +7,14 @@ var kamikaze_scene = load("res://enemies/kamikaze/kamikaze.tscn")
 var sniper_scene = load("res://enemies/sniper/Sniper.tscn")
 var rocketeer_scene = load("res://enemies/rocketeer/rocketeer.tscn")
 
-#var rambot_spawn_rate = 5
-#var rambot_spawn_time_remaining = 0
-#var shooter_spawn_rate = 5
-#var shooter_spawn_time_remaining = 0
-#var shooter_attack_rate = 1
-#var shooter_attack_time_remaining = 0
-#var kamikaze_spawn_rate = 5
-#var kamikaze_spawn_time_remaining = 0
-#var sniper_spawn_rate = 5
-#var sniper_spawn_time_remaining = 0
-#var sniper_attack_rate = 1
-#var sniper_attack_time_remaining = 0
-#var rocketeer_spawn_rate = 5
-#var rocketeer_spawn_time_remaining = 0
-#var rocketeer_attack_rate = 1
-#var rocketeer_attack_time_remaining = 0
+var rambot_spawn_number = "0"
+var kamikaze_spawn_number = "1"
+var shooter_spawn_number = "2"
+var sniper_spawn_number = "3"
+var rocketeer_spawn_number = "4"
+var shooter_attack_number = "5"
+var rocketeer_attack_number = "6"
+var sniper_attack_number = "7"
 
 var data = {}
 var rambot_spawn_time = 0
@@ -39,24 +31,16 @@ func _ready():
 	var file = File.new()
 	file.open("./data.txt", file.READ)
 	data.parse_json(file.get_as_text())
-	rambot_spawn_time = data["0"][0]
-	data["0"].remove(0)
-	shooter_spawn_time = data["4"][0]
-	data["4"].remove(0)
-	shooter_attack_time = data["5"][0]
-	data["5"].remove(0)
-	kamikaze_spawn_time = data["3"][0]
-	data["3"].remove(0)
-	sniper_spawn_time = data["4"][0]
-	data["4"].remove(0)
-	rocketeer_spawn_time = data["5"][0]
-	data["5"].remove(0)
-	rocketeer_attack_time = data["6"][0]
-	data["6"].remove(0)
-	#rambot_spawn_time = data["7"][0]
-	#data["7"].remove(0)
+	rambot_spawn_time = get_next_time(data, rambot_spawn_number)
+	kamikaze_spawn_time = get_next_time(data, kamikaze_spawn_number)
+	shooter_spawn_time = get_next_time(data, shooter_spawn_number)
+	sniper_spawn_time = get_next_time(data, sniper_spawn_number)
+	rocketeer_spawn_time = get_next_time(data, rocketeer_spawn_number)
+	shooter_attack_time = get_next_time(data, shooter_attack_number)
+	sniper_attack_time = get_next_time(data, sniper_attack_number)
+	rocketeer_attack_time = get_next_time(data, rocketeer_attack_number)
 	file.close()
-	get_node("SamplePlayer").play("sad")
+	get_node("SamplePlayer").play("anime")
 	set_fixed_process(true)
 
 func _fixed_process(delta):
@@ -64,56 +48,44 @@ func _fixed_process(delta):
 	player_scene.translate(Vector3(0, 0, -delta))
 	
 	time += delta
-	print(time)
-	if shooter_attack_time <= time:
-		get_tree().call_group(0, "shooter", "shoot")
-		shooter_attack_time = data["5"][0]
-		data["5"].remove(0)
+	if rambot_spawn_time <= time:
+		create_enemy(rambot_scene)
+		rambot_spawn_time = get_next_time(data, rambot_spawn_number)
+	
+	if kamikaze_spawn_time <= time:
+		create_enemy(kamikaze_scene)
+		kamikaze_spawn_time = get_next_time(data, kamikaze_spawn_number)
 	
 	if shooter_spawn_time <= time:
 		create_enemy(shooter_scene)
-		shooter_spawn_time = data["4"][0]
-		data["4"].remove(0)
+		shooter_spawn_time = get_next_time(data, shooter_spawn_number)
 	
-#	rambot_spawn_time_remaining -= delta
-#	if rambot_spawn_time_remaining <= 0:
-#		create_enemy(rambot_scene)
-#		rambot_spawn_time_remaining = rambot_spawn_rate
-#	
-#	shooter_spawn_time_remaining -= delta
-#	if shooter_spawn_time_remaining <= 0:
-#		create_enemy(shooter_scene)
-#		shooter_spawn_time_remaining = shooter_spawn_rate
-#	
-#	shooter_attack_time_remaining -= delta
-#	if shooter_attack_time_remaining <= 0:
-#		get_tree().call_group(0, "shooter", "shoot")
-#		shooter_attack_time_remaining = shooter_attack_rate
-#		
-#	sniper_spawn_time_remaining -= delta
-#	if sniper_spawn_time_remaining <= 0:
-#		create_enemy(sniper_scene)
-#		sniper_spawn_time_remaining = sniper_spawn_rate
-#	
-#	sniper_attack_time_remaining -= delta
-#	if sniper_attack_time_remaining <= 0:
-#		get_tree().call_group(0, "sniper", "shoot")
-#		sniper_attack_time_remaining = shooter_attack_rate
-#		
-#	rocketeer_spawn_time_remaining -= delta
-#	if rocketeer_spawn_time_remaining <= 0:
-#		create_enemy(rocketeer_scene)
-#		rocketeer_spawn_time_remaining = rocketeer_spawn_rate
-#	
-#	rocketeer_attack_time_remaining -= delta
-#	if rocketeer_attack_time_remaining <= 0:
-#		get_tree().call_group(0, "rocketeer", "shoot")
-#		rocketeer_attack_time_remaining = rocketeer_attack_rate
-#	
-#	kamikaze_spawn_time_remaining -= delta
-#	if kamikaze_spawn_time_remaining <= 0:
-#		create_enemy(kamikaze_scene)
-#		kamikaze_spawn_time_remaining = kamikaze_spawn_rate
+	if sniper_spawn_time <= time:
+		create_enemy(sniper_scene)
+		sniper_spawn_time = get_next_time(data, sniper_spawn_number)
+	
+	if rocketeer_spawn_time <= time:
+		create_enemy(rocketeer_scene)
+		rocketeer_spawn_time = get_next_time(data, rocketeer_spawn_number)
+	
+	if shooter_attack_time <= time:
+		get_tree().call_group(0, "shooter", "shoot")
+		shooter_attack_time = get_next_time(data, shooter_attack_number)
+	
+	if sniper_attack_time <= time:
+		get_tree().call_group(0, "sniper", "shoot")
+		sniper_attack_time = get_next_time(data, sniper_attack_number)
+	
+	if rocketeer_attack_time <= time:
+		get_tree().call_group(0, "rocketeer", "shoot")
+		rocketeer_attack_time = get_next_time(data, rocketeer_attack_number)
+
+func get_next_time(data, number):
+	var time = 999999999
+	if data[number].size() > 0:
+		time = data[number][0]
+		data[number].remove(0)
+	return time
 
 func create_enemy(enemy_scene):
 	var player_scene = get_node("player_scene")
