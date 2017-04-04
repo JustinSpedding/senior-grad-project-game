a3cae82cc78cc65648af85fa007a661361e7e3d7
Patrick Kibe <patrick9955@users.noreply.github.com>
2017-04-04 17:58:37 -0400

    start screen improvement with animation & preloading & bullet size increased dramatically

--- menus/StartGame.gd
+++ menus/StartGame.gd
@@ -1,7 +1,10 @@
 extends Button
 
+var scene;
+
 func _ready():
+	scene = preload("res://world_scene.tscn");
 	pass
 
 func _on_PlayGame_pressed():
-	get_tree().change_scene("res://world_scene.tscn")
+	get_tree().change_scene_to(scene);
