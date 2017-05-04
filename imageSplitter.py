ba200c9df0028967f5de6930837c611b85e3586b
askmiller <ajmiller2@mail.widener.edu>
2017-05-04 07:11:21 -0400

    added the 3 CNN files

---
+++ imageSplitter.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 17:07:04 2017
+
+@author: Andrew
+"""
+    
+def getSubImages(image):
+    #120 and 256
+    segments = []
+    for i in range(5):
+        for j in range(4):
+            segments.append(splitter(image, 256 * j, 120 * i, 120, 256))
+    return segments
+    
+def convertIndex(x, y):
+    newX = int(x) // 256
+    newY = int(y) // 120
+    index = (5 * newY) + newX
+    output = [0 for i in range(5 * 4)]
+    output[index] = 1
+    return output
+    
+def splitter(image, x, y, height, width):
+    segment = []
+    for i in range(height):
+        row = []
+        for j in range(width):
+            row.append(image[y + i][x + j])
+        segment.append(row)
+    return segment
--- tester.py
+++ tester.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 30 22:00:48 2017
+
+@author: Andrew
+"""
 from __future__ import division, print_function, absolute_import
 
 # Import tflearn and some helpers
 import tflearn
-from tflearn.data_utils import shuffle
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
@@ -10,31 +15,11 @@ from tflearn.data_preprocessing import ImagePreprocessing
 from tflearn.data_augmentation import ImageAugmentation
 from matplotlib.image import imread
 from os import listdir
-import imageSplitter
-
 # Load the data set
 dirs = [f for f in listdir("./ss/")]
 X = [imread("./ss/%s" % (f)) for f in dirs]
 Y = []
 dirs = [f for f in listdir("./loc/")]
-for d in dirs:
-    f = open("./loc/%s" % (d))
-    Y.append([y for y in f.read().split(' ')])
-print(len(X[0]), len(X[0][0]), len(X[0][0][0]))
-dirs = [f for f in listdir("./ss/")]
-X = []
-i = 0
-for d in dirs:
-    print(i)
-    i += 1
-    X.extend(imageSplitter.getSubImages(imread("./ss/%s" % (d))))
-Y = []
-dirs = [f for f in listdir("./loc/")]
-for d in dirs:
-    f = open("./loc/%s" % (d))
-    y = f.read().split(' ')
-    Y.extend(imageSplitter.convertIndex(float(y[0]), float(y[1])))
-    
 
 # Make sure the data is normalized
 img_prep = ImagePreprocessing()
@@ -43,15 +28,15 @@ img_prep.add_featurewise_stdnorm()
 
 # Create extra synthetic training data by flipping, rotating and blurring the
 # images on our data set.
-#img_aug = ImageAugmentation()
-#img_aug.add_random_flip_leftright()
-#img_aug.add_random_rotation(max_angle=25.)
-#img_aug.add_random_blur(sigma_max=3.)
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+img_aug.add_random_blur(sigma_max=3.)
 
 # Define our network architecture:
 
 # Input is a 32x32 image with 3 color channels (red, green and blue)
-network = input_data(shape=[None, 120, 256, 4],
+network = input_data(shape=[None, 600, 1024, 4],
                      data_preprocessing=img_prep)
 
 network = max_pool_2d(network, 4)
@@ -75,25 +60,13 @@ network = max_pool_2d(network, 2)
 network = fully_connected(network, 512, activation='relu')
 
 # Step 7: Dropout - throw away some data randomly during training to prevent over-fitting
-network = dropout(network, 0.5)
+#network = dropout(network, 0.5)
 
 # Step 8: Fully-connected neural network with two outputs (0=isn't a bird, 1=is a bird) to make the final prediction
-network = fully_connected(network, 1)
-
-# Tell tflearn how we want to train the network
-network = regression(network, optimizer='adam',
-                     loss='categorical_crossentropy',
-                     learning_rate=0.001)
+network = fully_connected(network, 2)
 
 # Wrap the network in a model object
 model = tflearn.DNN(network, tensorboard_verbose=0)
-
-# Train it! We'll do 100 training passes and monitor it as it goes.
-model.fit(X, Y, n_epoch=1000, shuffle=True,
-          show_metric=True, batch_size=96,
-          snapshot_epoch=True,
-          run_id='image-classifier')
-
-# Save model when training is complete to a file
-model.save("./cnn/image-classifier.tfl")
-print("Network trained and saved as image-classifier.tfl!")
+model.load("./cnn/image-classifier.tfl")
+model.predict(X)
+print(model.predict(X))
